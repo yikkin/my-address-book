c56f30de01b66838a846db800c9bdd887bf04781
yikkin <yongwe52@gmail.com>
2020-10-23 18:08:32 +0200

    first commit

---
+++ address_book/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render , redirect
+
+
+def home(request):
+	return render(request , "home.html" , {})
+
+def add_address(request):
+	return render(request , "add_address.html" , {})
+
+	
+# Create your views here.
